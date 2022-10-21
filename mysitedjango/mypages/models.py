afc26e152c0197732bc83e45ed1617ed8ce04abe
semfry <shaunfriend8@gmail.com>
2022-10-21 11:59:23 +0100

    fri1

--- mysitedjango/mypages/models.py
+++ mysitedjango/mypages/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 
+
 # Create your models here.
 
 
--- mysitedjango/mysitedjango/settings.py
+++ mysitedjango/mysitedjango/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_json_api",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "mysitedjango.urls"
@@ -162,3 +164,7 @@ REST_FRAMEWORK = {
 
 JSON_API_PLURALIZE_TYPES = True
 JSON_API_FORMAT_TYPES = 'camelize'
+
+CORS_ALLOWED_ORIGINS  = [
+    "http://localhost:4200",
+]
