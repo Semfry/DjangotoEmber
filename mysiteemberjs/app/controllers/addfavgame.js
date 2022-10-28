db252a8a187f966ce777427d27ca4cd02e61b0e3
semfry <shaunfriend8@gmail.com>
2022-10-28 11:28:55 +0100

    fri1

--- mysiteemberjs/app/controllers/addfavgame.js
+++ mysiteemberjs/app/controllers/addfavgame.js
@@ -2,25 +2,12 @@ import Controller from '@ember/controller';
 
 import { computed } from '@ember/object';
 
-// export default Controller.extend({  
+export default Controller.extend({  
     
-//     form: computed(function() {
-//         return {
-//             gamename: '',
-//             startyear: '',
-//        }
-//     })
-// });
-
-export default Controller.extend({
-    donutData: [
-      {
-        label: 'Super Cool',
-        data: 100
-      },
-      {
-        label: 'Very Cool',
-        data: 200
-      },
-    ],
-  });
+    form: computed(function() {
+        return {
+            gamename: '',
+            startyear: '',
+       }
+    })
+});
--- mysiteemberjs/app/controllers/modeltotalhours.js
+++ mysiteemberjs/app/controllers/modeltotalhours.js
@@ -1,28 +1,18 @@
 const { Controller } = Ember;
 
+// import Controller from '@ember/controller';
+
 import Ember from 'ember';
 
 import { computed } from '@ember/object';
 
-// export default Ember.Controller.extend({
-//     numberData: Ember.computed('model', function(){
-//         return {
-//             labels: this.get('model').mapBy('user'),
-//             datasets: [{
-//                 label: 'Total Hours',
-//                 data: this.get('model').mapBy('totalhours')
-//             }]
-//         }
-//     })
-// })
-
-export default Ember.Controller.extend({
-    numberData: Ember.computed('model', function() {
+export default Controller.extend({
+    numberData: computed('model', function(){
         return {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: this.get('model').mapBy('user'),
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                label: 'Total Hours',
+                data: this.get('model').mapBy('totalhours')
             }]
         }
     })
